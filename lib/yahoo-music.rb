b671360fe867b01a4578dee17a7b1307607b3603
Mattt Thompson <mail@matttthompson.com>
2008-08-07 16:24:15 -0700

    Added original source tree into repository Created unit tests for Artists and Releases

--- lib/yahoo-music.rb
+++ lib/yahoo-music.rb
@@ -1,7 +1,24 @@
+%w{rubygems cgi hpricot activesupport}.each { |x| require x }
+
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
+require 'rest'
+require 'yahoo-music/base'
+require 'yahoo-music/version'
+
+require 'yahoo-music/artist'
+require 'yahoo-music/category'
+require 'yahoo-music/release'
+require 'yahoo-music/review'
+require 'yahoo-music/track'
+require 'yahoo-music/video'
+
 module Yahoo
   module Music
+    LOCALE      = "us"
+    API_URL     = "http://#{LOCALE}.music.yahooapis.com/"
+    API_VERSION = 'v1'
+    APP_ID      = "25BJGafV34GnIar0alwCNe6VkJfrSe4.FKqHZlzwM73lP5aCWu4K48eUEVszu3dI7aPcTwc-"    
   end
 end
